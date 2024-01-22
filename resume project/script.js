09cb3696a7889cf9d6a5c7d254f851e4add7ed2f
Nishanth <140162104+NishanthRtc@users.noreply.github.com>
2024-01-22 05:01:38 -0500

    Add files via upload

---
+++ resume project/script.js
@@ -0,0 +1,25 @@
+const sounds = ["poradalam", "kannazhaga", "Theetriyaai", "Deva", "Po Indru", "Idhazhin"];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+
+        document.getElementById(sound).play();
+    });
+
+    document.body.appendChild(btn);
+});
+
+function stopSongs() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    });
+}
